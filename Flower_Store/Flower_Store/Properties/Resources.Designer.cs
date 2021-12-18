f56d8f14711ba639f5ceb00fbffd382a1bea2aa7
thanghoang <hoanganhthang357@gmail.com>
2021-12-18 16:59:11 +0700

    Add Report Form

--- Flower_Store/Flower_Store/Properties/Resources.Designer.cs
+++ Flower_Store/Flower_Store/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Flower_Store.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap business {
+            get {
+                object obj = ResourceManager.GetObject("business", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap close {
             get {
                 object obj = ResourceManager.GetObject("close", resourceCulture);
---
+++ Flower_Store/Flower_Store/Report.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Flower_Store
+{
+    public partial class Report : Form
+    {
+        public Report()
+        {
+            InitializeComponent();
+        }
+    }
+}
